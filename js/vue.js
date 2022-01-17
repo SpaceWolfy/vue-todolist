5222c5f0e76a332efb0edef19cfe78f7040490cd
Samuel Scozzari <s_scozzari@libero.it>
2022-01-17 15:55:26 +0100

    Exercise + bonuses completed

---
+++ js/vue.js
@@ -0,0 +1,49 @@
+new Vue({
+  el: "#app",
+  data: {
+    list: [
+      {
+        text: "Fare i compiti",
+        done: true,
+      },
+      {
+        text: "Lavare i piatti",
+        done: false,
+      },
+      {
+        text: "Fare il bucato",
+        done: true,
+      },
+      {
+        text: "Pagare le Boolette",
+        done: false,
+      },
+    ],
+    //per l'input
+    addNewItem: "",
+    conterClick: 0,
+  },
+  methods: {
+    addItemToList: function () {
+      let upperCItem =
+        this.addNewItem.charAt(0).toUpperCase() + this.addNewItem.slice(1);
+      if (this.addNewItem === "") {
+        alert("Prima di inviare, inserisci qualcosa!");
+      } else {
+        this.list.push({ text: upperCItem, done: false });
+        this.addNewItem = "";
+      }
+    },
+    removeItemFromList: function (index) {
+      console.log("Rimuovo l'elemento di indice", index);
+      this.list.splice(index, 1); //rimuove un elemento all'indice indicato
+    },
+    cancelItemInList: function (index) {
+      if (this.list[index].done) {
+        this.list[index].done = false;
+      } else {
+        this.list[index].done = true;
+      }
+    },
+  },
+});
